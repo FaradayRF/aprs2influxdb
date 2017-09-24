e1affa429bde03ff0e1b8401614d3dc8324447f5
kb1lqc <bryce@faradayrf.com>
2017-09-24 07:39:37 -0700

    Added configuration INI and influxdb connection
    Connect to influxdb with INI file values.

--- clerk.py
+++ clerk.py
@@ -1,16 +1,36 @@
 import aprslib
+import configparser
+from influxdb import InfluxDBClient
 
 def callback(packet):
     packet = aprslib.parse(packet)
     if packet['to'] == 'GPSFDY':
         print packet
 
+def connectInfluxDB():
+
+    config = configparser.ConfigParser()
+    config.read('clerk.ini')
+
+    host = config['influx']['host']
+    port = config['influx']['port']
+    user = config['influx']['user']
+    password = config['influx']['password']
+    dbname = config['influx']['dbname']
+    dbuser = config['influx']['dbuser']
+    dbuser_password = config['influx']['dbuserpassword']
+
+    client = InfluxDBClient(host, port, user, password, dbname)
+    print client
+
 def main():
     # Open APRS-IS connection
     AIS = aprslib.IS("KB1LQC")
     AIS.connect()
+    connectInfluxDB()
     # Obtain raw APRS-IS packets and sent to callback when received
     AIS.consumer(callback, raw=True)
+    print "woot"
 
 if __name__ == "__main__":
     main()
