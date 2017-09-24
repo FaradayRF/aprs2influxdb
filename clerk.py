86c252175ce169385466d6886002e45d2e449f07
kb1lqc <bryce@faradayrf.com>
2017-09-24 07:22:41 -0700

    Initial aprslib parsing for GPSFDY
    Simply parsing APRS-IS for GPSFDY packets.

--- clerk.py
+++ clerk.py
@@ -1,7 +1,16 @@
 import aprslib
 
+def callback(packet):
+    packet = aprslib.parse(packet)
+    if packet['to'] == 'GPSFDY':
+        print packet
+
 def main():
-    pass
+    # Open APRS-IS connection
+    AIS = aprslib.IS("KB1LQC")
+    AIS.connect()
+    # Obtain raw APRS-IS packets and sent to callback when received
+    AIS.consumer(callback, raw=True)
 
 if __name__ == "__main__":
     main()
