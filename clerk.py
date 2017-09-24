7215b7f8639f23a23711727c94501877a3766ec8
kb1lqc <bryce@faradayrf.com>
2017-09-24 16:34:45 -0700

    Redesigned schema and parsing of JSON data
    Much better and thorough schema design with way more efficient creation of
    the line protocol string. This just prints the string to the screen to
    test how it works. Also, only performs this on uncompressed format APRS
    messages.

--- clerk.py
+++ clerk.py
@@ -5,25 +5,67 @@ from influxdb import InfluxDBClient
 import Geohash
 
 def jsonToLineProtocol(jsonData):
-    #Converts aprslib JSON to influxdb line protocol
+    # Converts aprslib JSON to influxdb line protocol
+    # Schema
+    # measurement = packet
+    # tag = from
+    # tag = to
+    # tag = symbolTable
+    # tag = symbol
+    # tag = format
+    # tag = comment
+    # field = latitude
+    # field = longitude
+    # field = posAmbiguity
+    # field = altitude
+    # field = speed
+    # field = sequenceNumber
+    # field = analog1
+    # field = analog2
+    # field = analog3
+    # field = analog4
+    # field = analog5
+    # field = digital
 
-    line = "packets,from={0},geohash={9} latitude={1},longitude={2},altitude={3},analog0={4},analog1={5},analog2={6},analog3={7},analog4={8}"
+    if jsonData["format"] == "uncompressed":
+        # initialize variables
+        tags = []
+        fields = []
 
-    try:
-        #geohashVal = ''
-        geohashVal =  Geohash.encode(jsonData["latitude"],jsonData["longitude"])
-        geohashVal = "\"" + geohashVal + "\""
-        #print geohashVal
+        measurement = "packet"
+        tags.append("from={0}".format(jsonData["from"])
+        tags.append("to={0}".format(jsonData["to"])
+        tags.append("symbolTable={0}".format(jsonData["symbol_table"])
+        tags.append("symbol={0}".format(jsonData["symbol"])
+        tags.append("format={0}".format(jsonData["format"])
+        tags.append("comment={0}".format(jsonData["comment"])
+        tagStr = ",".join(tags)
 
-    except StandardError as e:
-        print e
+        fields.append("latitude={0}".format(jsonData["latitude"])
+        fields.append("longitude={0}".format(jsonData["longitude"])
+        fields.append("posAmbiguity={0}".format(jsonData["posambiguity"])
+        fields.append("altitude={0}".format(jsonData["altitude"])
+        fields.append("speed={0}".format(jsonData["speed"])
+        fields.append("sequenceNumber={0}".format(jsonData["seq"])
+        fields.append("analog1={0}".format(jsonData["telemetry"]["vals"][0])
+        fields.append("analog2={0}".format(jsonData["telemetry"]["vals"][1])
+        fields.append("analog3={0}".format(jsonData["telemetry"]["vals"][2])
+        fields.append("analog4={0}".format(jsonData["telemetry"]["vals"][3])
+        fields.append("analog5={0}".format(jsonData["telemetry"]["vals"][4])
+        fields.append("digital={0}".format(jsonData["digital"])
+        fieldsStr = ",".join(fields)
 
-    try:
-        a = line.format(jsonData["from"],jsonData["latitude"],jsonData["longitude"],jsonData["altitude"],jsonData["telemetry"]["vals"][0],jsonData["telemetry"]["vals"][1],jsonData["telemetry"]["vals"][2],jsonData["telemetry"]["vals"][3],jsonData["telemetry"]["vals"][4],str(geohashVal))
-        print a
-	return a
-    except StandardError as e:
-        pass
+        lineProtocolStr = " ".join(measurement,tagStr,fieldsStr)
+        print lineProtocolStr
+
+    #line = "packet,from={0},geohash={9} latitude={1},longitude={2},altitude={3},analog0={4},analog1={5},analog2={6},analog3={7},analog4={8}"
+
+    # try:
+    #     a = line.format(jsonData["from"],jsonData["latitude"],jsonData["longitude"],jsonData["altitude"],jsonData["telemetry"]["vals"][0],jsonData["telemetry"]["vals"][1],jsonData["telemetry"]["vals"][2],jsonData["telemetry"]["vals"][3],jsonData["telemetry"]["vals"][4],str(geohashVal))
+    #     print a
+	# return a
+    # except StandardError as e:
+    #     pass
 
     #except InfluxDB.ClientError as e:
     #    print e
