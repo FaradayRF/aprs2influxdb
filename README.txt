e2676778340a16343034950f9755016d534b81ed
kb1lqc <bryce@faradayrf.com>
2017-09-27 21:41:26 -0700

    Moving towards normal setuptools packaging
    Following The Hitchhiker's Guide to Packaging

---
+++ README.txt
@@ -0,0 +1,6 @@
+===========
+aprs2influxdb
+===========
+
+aprs2influxdb provides an APRS-IS client which extracts APRS messages and
+saves them to an influxdb time series database
--- setup.py
+++ setup.py
@@ -1,8 +1,27 @@
-#!/usr/bin/env python
-
-from setuptools import setup
+from distutils.core import setup
 
 setup(
-    setup_requires=['pbr', 'setuptools'],
-    pbr=True,
+    name='APRS2InfluxDB',
+    version='0.1.0',
+    author='Bryce Salmi',
+    author_email='Bryce@FaradayRF.com',
+    packages=['aprs2influxdb'],
+    url='https://github.com/FaradayRF/aprs2influxdb',
+    license='GPLv3',
+    description='Connects to APRS-IS and saves packets to InfluxDB',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "aprslib>=0.6.46",
+        "certifi>=2017.7.27.1",
+        "chardet>=3.0.4",
+        "configparser>=3.5.0",
+        "idna>=2.6",
+        "influxdb>=4.1.1",
+        "pbr>=3.1.1",
+        "python-dateutil>=2.6.1",
+        "pytz>=2017.2",
+        "requests>=2.18.4",
+        "six>=1.11.0",
+        "urllib3>=1.22",
+    ],
 )
