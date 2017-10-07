d3532552841acbf53e4ba2b414841420b79e91ce
Bryce Salmi <bryce@faradayrf.com>
2017-10-07 00:24:45 -0700

    Updated version and README per #13

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aprs2influxdb',
-    version='0.1.0',
+    version='0.1.1',
     author='Bryce Salmi',
     author_email='Bryce@FaradayRF.com',
     packages=['aprs2influxdb'],
