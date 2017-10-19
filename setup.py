73a182f343f7d2fce44d6fc7644a50725bd150a2
Bryce Salmi <bryce@faradayrf.com>
2017-10-18 23:38:56 -0700

    Changed version to 0.2.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='aprs2influxdb',
-    version='0.1.1',
+    version='0.2.0',
     author='Bryce Salmi',
     author_email='Bryce@FaradayRF.com',
     packages=['aprs2influxdb'],
